490e5011b325b3b6817b3262ff2a9590b814a47b
Rahul Dey <rahuldey832000@gmail.com>
2023-10-02 15:39:47 +0530

    Add Dotnet Project

---
+++ LearningDotnetAgain/LearningDotnetAgain/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(MyEnum.Active.ToString());
+
+MyEnum value = MyEnum.Active;
+Console.WriteLine(value.ToString());
