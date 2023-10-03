29258b1ed98601f0d31518ad386ab7561a47ecae
Rahul Dey <rahuldey832000@gmail.com>
2023-10-03 19:47:24 +0530

    Check for each on list items

---
+++ LearningDotnetAgain/LearningDotnetAgain/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace LearningDotnetAgain
+{
+    public class Person
+    {
+        public Person(string name, int age, string description = "")
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Description = description;
+        }
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Age { get; set; } = 0;
+    }
+}
--- LearningDotnetAgain/LearningDotnetAgain/Program.cs
+++ LearningDotnetAgain/LearningDotnetAgain/Program.cs
@@ -1,7 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LearningDotnetAgain;
+
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine(MyEnum.Active.ToString());
+// Console.WriteLine(MyEnum.Active.ToString());
 
-MyEnum value = MyEnum.Active;
-Console.WriteLine(value.ToString());
+// MyEnum value = MyEnum.Active;
+// Console.WriteLine(value.ToString());
+
+List<Person> list = new();
+
+list.Add(new Person("Rahul", 22, "CSE-49"));
+list.Add(new Person("Person1", 24, "CSE-86"));
+list.Add(new Person("Prabir", 23, "CSE-50"));
+
+foreach (var item in list)
+{
+    if(item.Age == 23)
+    {
+        item.Description = "You are a Senior";
+    }
+}
+
+
+foreach (Person person in list)
+{
+    Console.WriteLine($"{person.Name} : {person.Age} -> {person.Description}");
+}
