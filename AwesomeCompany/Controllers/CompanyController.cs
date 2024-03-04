dfb850ed80740cb5595d798edf7b7e842e72bab5
Rahul Dey <rahuldey832000@gmail.com>
2024-03-04 18:25:01 +0530

    Improved EF Update operation

--- AwesomeCompany/Controllers/CompanyController.cs
+++ AwesomeCompany/Controllers/CompanyController.cs
@@ -29,6 +29,7 @@ public class CompanyController(AppDbContext context) : ControllerBase
     [Route("increase-salaries/{companyId:int}")]
     public async Task<IActionResult> UpdateSalaries(int companyId)
     {
+        // Entity framework core create 1000 sql update command to perform this operation 🙀🙀
         var companyDetails = await _context.Set<Company>().Include(c => c.Employees).FirstOrDefaultAsync(c => c.Id == companyId);
 
         if (companyDetails == null)
@@ -43,7 +44,34 @@ public class CompanyController(AppDbContext context) : ControllerBase
 
         companyDetails.LastSalaryUpdateUtc = DateTime.UtcNow;
 
+        await _context.SaveChangesAsync(); // 1 more sql command -> total 1001 sql commands 🙀🙀
+
+        return NoContent();
+    }
+
+    [HttpPut]
+    [Route("increase-salaries-sql/{companyId:int}")]
+    public async Task<IActionResult> UpdateSalariesSql(int companyId)
+    {
+        var companyDetails = await _context.Set<Company>().Include(c => c.Employees).FirstOrDefaultAsync(c => c.Id == companyId);
+
+        if (companyDetails == null)
+        {
+            return NotFound();
+        }
+
+        // begin database transaction
+        await _context.Database.BeginTransactionAsync();
+
+        // performing row sql query 
+        await _context.Database.ExecuteSqlInterpolatedAsync($"UPDATE Employees SET Salary = Salary * 1.1 WHERE CompanyId = {companyDetails.Id}");
+
+        companyDetails.LastSalaryUpdateUtc = DateTime.UtcNow;
+
         await _context.SaveChangesAsync();
+
+        // commit database transaction
+        await _context.Database.CommitTransactionAsync();
 
         return NoContent();
     }
