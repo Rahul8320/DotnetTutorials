d6f30ad084b3592690ee81381326a3bb48898530
Rahul Dey <rahuldey832000@gmail.com>
2024-03-07 11:11:54 +0530

    Add Architecture Tests

--- CleanArchitecture/test/Architecture.Tests/ArchitectureTests.cs
+++ CleanArchitecture/test/Architecture.Tests/ArchitectureTests.cs
@@ -1,3 +1,6 @@
+using FluentAssertions;
+using NetArchTest.Rules;
+
 namespace Architecture.Tests;
 
 public class ArchitectureTests
@@ -9,8 +12,116 @@ public class ArchitectureTests
     private const string WebNamespace = "Web";
 
     [Fact]
-    public void Test1()
+    public void Domain_Should_Not_HaveDependencyOnOtherProjects()
     {
+        // Arrange
+        var assembly = typeof(Domain.AssemblyReference).Assembly;
 
+        var otherProjects = new[]{
+            ApplicationNamespace,
+            InfrastructureNamespace,
+            PresentationNamespace,
+            WebNamespace
+        };
+
+        // Act
+        var testResult = Types.InAssembly(assembly).ShouldNot().HaveDependencyOnAll(otherProjects).GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Application_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        // Arrange
+        var assembly = typeof(Application.AssemblyReference).Assembly;
+
+        var otherProjects = new[]{
+            InfrastructureNamespace,
+            PresentationNamespace,
+            WebNamespace
+        };
+
+        // Act
+        var testResult = Types.InAssembly(assembly).ShouldNot().HaveDependencyOnAll(otherProjects).GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Handlers_Should_Have_DependencyOnDomain()
+    {
+        // Arrange
+        var assembly = typeof(Application.AssemblyReference).Assembly;
+
+        // Act
+        var testResult = Types
+            .InAssembly(assembly)
+            .That()
+            .HaveNameEndingWith("Handler")
+            .Should()
+            .HaveDependencyOn(DomainNamespace)
+            .GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+
+
+    [Fact]
+    public void Infrastructure_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        // Arrange
+        var assembly = typeof(Infrastructure.AssemblyReference).Assembly;
+
+        var otherProjects = new[]{
+            PresentationNamespace,
+            WebNamespace
+        };
+
+        // Act
+        var testResult = Types.InAssembly(assembly).ShouldNot().HaveDependencyOnAll(otherProjects).GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Presentation_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        // Arrange
+        var assembly = typeof(Presentation.AssemblyReference).Assembly;
+
+        var otherProjects = new[]{
+            InfrastructureNamespace,
+            WebNamespace
+        };
+
+        // Act
+        var testResult = Types.InAssembly(assembly).ShouldNot().HaveDependencyOnAll(otherProjects).GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Controller_Should_Have_DependencyOnMediatR()
+    {
+        // Arrange
+        var assembly = typeof(Presentation.AssemblyReference).Assembly;
+
+        // Act
+        var testResult = Types
+            .InAssembly(assembly)
+            .That()
+            .HaveNameEndingWith("Controller")
+            .Should()
+            .HaveDependencyOn("MediatR")
+            .GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
     }
 }
