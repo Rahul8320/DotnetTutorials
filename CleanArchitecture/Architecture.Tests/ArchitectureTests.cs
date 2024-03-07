0ba91cfeb379a9c3e53a75efbfe9c0f2d8a61ff0
Rahul Dey <rahuldey832000@gmail.com>
2024-03-07 10:27:34 +0530

    Add Architecture Tests Project

---
+++ CleanArchitecture/Architecture.Tests/ArchitectureTests.cs
@@ -0,0 +1,16 @@
+namespace Architecture.Tests;
+
+public class ArchitectureTests
+{
+    private const string DomainNamespace = "Domain";
+    private const string ApplicationNamespace = "Application";
+    private const string InfrastructureNamespace = "Infrastructure";
+    private const string PresentationNamespace = "Presentation";
+    private const string WebNamespace = "Web";
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
