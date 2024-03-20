5f9a6ae37c326a23af220e6b1b69c8f5b25383cd
Rahul Dey <rahuldey832000@gmail.com>
2024-03-20 19:41:23 +0530

    Create db context

---
+++ EfCoreRelationships/EfCoreRelationShips.WebApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreRelationShips.WebApi;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+}
