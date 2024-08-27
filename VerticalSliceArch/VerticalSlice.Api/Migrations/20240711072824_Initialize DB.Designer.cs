8accaa17c6f91a79975810c2077979a1d2504b9f
Rahul Dey <rahuldey832000@gmail.com>
2024-08-28 00:19:40 +0530

    Add User Table Migrations

--- VerticalSliceArch/VerticalSlice.Api/Migrations/20240711072824_Initialize DB.Designer.cs
+++ VerticalSliceArch/VerticalSlice.Api/Migrations/20240711072824_Initialize DB.Designer.cs
@@ -3,7 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using VerticalSlice.Api.Products.Data;
+using VerticalSlice.Api.Data;
+
 
 #nullable disable
 
